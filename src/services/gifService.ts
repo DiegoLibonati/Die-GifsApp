7bbf5029ac2774e743356fb890e2c3f02121b670
Die <dlibonati@leafnoise.io>
2026-05-01 20:11:29 -0300

    feat: better response type

--- src/services/gifService.ts
+++ src/services/gifService.ts
@@ -1,66 +1,26 @@
 import type { Gif } from "@/types/app";
+import type { ResponseMetaData } from "@/types/responses";
 
 import envs from "@/constants/envs";
 
 const API_KEY = envs.VITE_API_KEY;
 
 const gifService = {
-  getAll: async (
-    category: string,
-    numberOfGifs: number
-  ): Promise<{
-    data: Gif[];
-    meta: {
-      status: number;
-      msg: string;
-      response_id: string;
-    };
-    pagination: {
-      total_count: number;
-      count: number;
-      offset: number;
-    };
-  }> => {
+  getAll: async (category: string, numberOfGifs: number): Promise<ResponseMetaData<Gif[]>> => {
     const response = await fetch(
       `/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${numberOfGifs}&offset=0&rating=r&lang=en`
     );
 
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    return (await response.json()) as {
-      data: Gif[];
-      meta: {
-        status: number;
-        msg: string;
-        response_id: string;
-      };
-      pagination: {
-        total_count: number;
-        count: number;
-        offset: number;
-      };
-    };
+    return (await response.json()) as ResponseMetaData<Gif[]>;
   },
-  getRandomGifsByCategory: async (): Promise<{
-    data: Gif;
-    meta: {
-      status: number;
-      msg: string;
-      response_id: string;
-    };
-  }> => {
+  getRandomGifsByCategory: async (): Promise<ResponseMetaData<Gif>> => {
     const response = await fetch(`/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`);
 
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    return (await response.json()) as {
-      data: Gif;
-      meta: {
-        status: number;
-        msg: string;
-        response_id: string;
-      };
-    };
+    return (await response.json()) as ResponseMetaData<Gif>;
   },
 };
 
--- src/types/responses.ts
+++ src/types/responses.ts
@@ -6,3 +6,19 @@ export interface DefaultResponse {
 export interface ResponseWithData<T> extends DefaultResponse {
   data: T;
 }
+
+export type ResponseDirect<T> = T;
+
+export interface ResponseMetaData<T> {
+  data: T;
+  meta?: {
+    status: number;
+    msg: string;
+    response_id: string;
+  };
+  pagination?: {
+    total_count: number;
+    count: number;
+    offset: number;
+  };
+}
